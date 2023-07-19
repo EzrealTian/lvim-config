64114db025dca4f47b022c89d9ebe0aeb032fb41
EzrealTian <tyx1115763021@icloud.com>
2023-07-19 16:42:00 +0800

    add cscope

--- config.lua
+++ config.lua
@@ -80,6 +80,22 @@ lvim.plugins = {
       })
     end,
   },
+  {
+    "dhananjaylatkar/cscope_maps.nvim",
+    dependencies = {
+      "folke/which-key.nvim",          -- optional [for whichkey hints]
+      "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+      "ibhagwan/fzf-lua",              -- optional [for picker="fzf-lua"]
+      "nvim-tree/nvim-web-devicons",   -- optional [for devicons in telescope or fzf]
+    },
+    opts = {
+      cscope = {
+        picker = "telescope"
+      }
+      -- USE EMPTY FOR DEFAULT OPTIONS
+      -- DEFAULTS ARE LISTED BELOW
+    },
+  }
 }
 
 -- plugin config
--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,6 +1,6 @@
 -- key mapping
 lvim.keys.normal_mode["<Leader>rn"] = "<Cmd>lua vim.lsp.buf.rename()<CR>"
-lvim.keys.normal_mode["<S-l>"] = "<Cmd>BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = "<Cmd>BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["<S-c>"] = "<Cmd>BufferKill<CR>"
 lvim.keys.normal_mode["<Leader>l"] = "<Cmd>Lazy<CR>"
+lvim.builtin.which_key.mappings['c'] = {}
+
+lvim.builtin.bufferline.keymap.normal_mode["<Leader>bc"] = ":BufferKill<CR>"
